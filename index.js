1a4ef243c1efb4cf398a23192c682081ae8f4763
Brent Reeves <brent@twinbear.com>
2021-03-18 12:19:25 -0500

    ssl: this.isProduction() ? { rejectUnauthorized: false } : false

--- index.js
+++ index.js
@@ -12,30 +12,32 @@ const { Pool } = require("pg");
 console.log("index.js starting...\n");
 
 // heroku
- const pool = new Pool({
-   connectionString: process.env.DATABASE_URL,
-     ssl: { rejectUnauthorized: false }
-//   ssl: true
- });
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: this.isProduction() ? { rejectUnauthorized: false } : false
+    //     ssl: { rejectUnauthorized: false }
+    //   ssl: true
+});
 // heroku end
 
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get("/db", async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const sql = `select * from ${TABLE1}`;
-      const result = await client.query(sql);
-      const results = { results: result ? result.rows : null };
-      res.render("pages/db", results);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error db " + err);
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get("/db", async (req, res) => {
+	console.log('table ' + TABLE1);
+	try {
+	    const client = await pool.connect();
+	    const sql = `select * from ${TABLE1}`;
+	    const result = await client.query(sql);
+	    const results = { results: result ? result.rows : null };
+	    res.render("pages/db", results);
+	    client.release();
+	} catch (err) {
+	    console.error(err);
+	    res.send("Error db " + err);
+	}
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
